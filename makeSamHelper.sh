9e4484ac4de9becec3b061e8470d07f876c24f29
John Felleman <john.felleman@gsa.gov>
2013-12-19 17:09:09 -0800

    work on makeHelper script file

--- makeSamHelper.sh
+++ makeSamHelper.sh
@@ -1,4 +1,4 @@
-!/bin/sh
+#/bin/sh
 
 cp manifest.helperExtension.json manifest.json
-zip samtracker.zip ./Shared/* ./SamHelper/* ./Images/* ./ChromeExtension.adapter/* ./ChromeExtension.adapter/images/*  ./manifest.json
+zip samHelper.zip ./Shared/* ./SamHelper/* ./Images/* ./ChromeExtension.adapter/* ./ChromeExtension.adapter/images/*  ./manifest.json
